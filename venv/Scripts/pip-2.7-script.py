1ed0312b268512427fce2842cf6d5f8267841fd1
Shivam Thapar <shivam.thapar1@gmail.com>
2012-12-24 12:45:22 -0800

    PLEASE WORK

--- venv/Scripts/pip-2.7-script.py
+++ venv/Scripts/pip-2.7-script.py
@@ -1,10 +1,10 @@
 #!C:\Users\shivam\Documents\Django\tunetype\venv\Scripts\python2.7.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'pip==1.2.1','console_scripts','pip-2.7'
-__requires__ = 'pip==1.2.1'
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==1.2','console_scripts','pip-2.7'
+__requires__ = 'pip==1.2'
 import sys
 from pkg_resources import load_entry_point
 
 if __name__ == '__main__':
     sys.exit(
-        load_entry_point('pip==1.2.1', 'console_scripts', 'pip-2.7')()
+        load_entry_point('pip==1.2', 'console_scripts', 'pip-2.7')()
     )
