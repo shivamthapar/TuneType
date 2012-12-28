4627815fb21feaa955236581331975dd7758e1b5
Shivam Thapar <shivam.thapar1@gmail.com>
2012-12-27 23:48:45 -0800

    replaced lyriccheck with training

--- lyriccheck/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('lyriccheck.views',
-    url(r'^$', 'index'),
-    url(r'^(?P<artist>[a-zA-Z0-9\-+]+)/(?P<song>[a-zA-Z0-9\-+]+)/lyrics/$', 'lyrics'),
-
-)
